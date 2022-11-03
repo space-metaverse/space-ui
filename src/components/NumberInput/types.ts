c46e342160f8fd69ab06c17a650dfc76ebfa57a1
Yuri Martins <develop@yumartins.com.br>
2022-11-03 19:19:28 -0300

    fix(number-input): generic types

--- src/components/NumberInput/types.ts
+++ src/components/NumberInput/types.ts
@@ -1,6 +1,6 @@
-import type { ChangeEvent, HTMLAttributes } from 'react';
+import type { ChangeEvent, InputHTMLAttributes } from 'react';
 
-export interface NumberInputProps extends HTMLAttributes<HTMLDivElement> {
+export interface NumberInputProps extends InputHTMLAttributes<HTMLInputElement> {
     label?: string;
     value?: number;
     isError?: boolean;
