b679b9f26b1c187cc0580826b065920af14ba9ce
Matheus <matheusluciox@gmail.com>
2022-11-20 18:03:28 -0300

    Change type to accept jsx element and strings

--- src/components/Checkbox/types.ts
+++ src/components/Checkbox/types.ts
@@ -1,7 +1,8 @@
-import type { ChangeEvent, LabelHTMLAttributes } from 'react';
+import type { ChangeEvent, LabelHTMLAttributes } from "react";
 
-export interface CheckboxProps extends Omit<LabelHTMLAttributes<HTMLLabelElement>, 'onChange'> {
-    label?: string;
+export interface CheckboxProps
+    extends Omit<LabelHTMLAttributes<HTMLLabelElement>, "onChange"> {
+    label?: string | Element;
     disabled?: boolean;
     isChecked: boolean;
     onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
