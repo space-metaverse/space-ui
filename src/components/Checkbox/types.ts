2fd55588cdce4ce18cd4fc1d5f28bfbc1b8a57ce
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-11-21 16:24:27 -0700

    fix checkbox type

--- src/components/Checkbox/types.ts
+++ src/components/Checkbox/types.ts
@@ -2,7 +2,7 @@ import type { ChangeEvent, LabelHTMLAttributes } from "react";
 
 export interface CheckboxProps
     extends Omit<LabelHTMLAttributes<HTMLLabelElement>, "onChange"> {
-    label?: string | Element;
+    label?: string | React.ReactNode;
     disabled?: boolean;
     isChecked: boolean;
     onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
